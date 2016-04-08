fec9f794e67f16c2ed4b012b9fd34b659cc14644
Thomas Schorn <thomas@schorn.io>
2016-04-08 18:38:49 +0200

    [minor] handlebars support

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const express = require('express');
 
 let app = express();
 
+app.set('view engine', 'hbs');
+
 app.use('/static', express.static(`${__dirname}/static`));
 
 app.get('/', (request, response) => {
-  response.sendFile(`${__dirname}/templates/index.html`);
+  response.render('index.hbs', {});
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
